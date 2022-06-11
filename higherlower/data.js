36f90da61ac0fe43b0dc03b68cfe3343b011c1af
Haunting2304 <60801196+Haunting2304@users.noreply.github.com>
2022-06-11 02:25:57 -0700

    Added homepage

--- higherlower/data.js
+++ higherlower/data.js
@@ -2,36 +2,36 @@ var gameItems = [
     {
         "name": "Canada",
         "value": 38.01,
-        "url": "images/Canada.jpg"
+        "url": "../images/Canada.jpg"
     },
     {
         "name": "United States",
         "value": 329.50,
-        "url": "images/US.jpg"
+        "url": "../images/US.jpg"
     },
     {
         "name": "Brazil",
         "value": 212.60,
-        "url": "images/Brazil.jpg"
+        "url": "../images/Brazil.jpg"
     },
     {
         "name": "China",
         "value": 1450,
-        "url": "images/China.jpg"
+        "url": "../images/China.jpg"
     },
     {
         "name": "India",
         "value": 1400,
-        "url": "images/India.jpg"
+        "url": "../images/India.jpg"
     },
     {
         "name": "United Kingdom",
         "value": 67.08,
-        "url": "images/UK.jpg"
+        "url": "../images/UK.jpg"
     },
     {
         "name": "South Korea",
         "value": 51.63,
-        "url": "images/SouthKorea.jpg"
+        "url": "../images/SouthKorea.jpg"
     }
 ];
